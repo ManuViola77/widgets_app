800f99993734eba82d37ff3a6b335034a5736201
Manuela Viola <manuela.viola@gmail.com>
2025-05-22 14:29:57 -0300

    feat: card with outline

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -36,7 +36,7 @@ class _CardsView extends StatelessWidget {
           ),
           ...cards.map(
             (card) =>
-                _CardType1(label: card['label'], elevation: card['elevation']),
+                _CardType2(label: card['label'], elevation: card['elevation']),
           ),
         ],
       ),
@@ -66,6 +66,44 @@ class _CardType1 extends StatelessWidget {
               ),
             ),
             Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        side: BorderSide(color: colors.outline),
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
+            ),
           ],
         ),
       ),
