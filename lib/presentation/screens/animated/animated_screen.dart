51c7e4caefee55a7f14aef619a55bf033b55e1e2
Manuela Viola <manuela.viola@gmail.com>
2025-05-23 00:53:24 -0300

    feat: star animated component

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -7,6 +7,24 @@ class AnimatedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Animated')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Animated Container')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.play_arrow_rounded),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.easeOutCubic,
+          width: 200,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
   }
 }
