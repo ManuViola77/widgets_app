c94704ab376f67dc20a6db3bda9611b4cd3d41e1
Manuela Viola <manuela.viola@gmail.com>
2025-05-23 16:11:24 -0300

    feat: start list view with builder and remove top and botton padding

--- lib/menu/menu_items.dart
+++ lib/menu/menu_items.dart
@@ -57,4 +57,10 @@ const appMenuItems = <MenuItem>[
     link: '/tutorial',
     icon: Icons.accessible_rounded,
   ),
+  MenuItem(
+    title: 'Infinite Scroll y Pull to Refresh',
+    subTitle: 'Listas infinitas y pull to refresh',
+    link: '/infinite-scroll',
+    icon: Icons.list_alt_rounded,
+  ),
 ];
--- lib/presentation/screens/animated/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/animated/infinite_scroll/infinite_scroll_screen.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
   static const String name = 'infinite_scroll_screen';
 
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  List<int> imagesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Infinite Scroll')));
+    return Scaffold(
+      backgroundColor: Colors.black,
+      // media query para quitar el padding de la appbar
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          itemCount: imagesIds.length,
+          itemBuilder: (context, index) {
+            return FadeInImage(
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                'https://picsum.photos/id/${imagesIds[index]}/500/300',
+              ),
+              placeholder: const AssetImage('assets/images/jar-loading.gif'),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pop(),
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+      ),
+    );
   }
 }
